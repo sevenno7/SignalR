c34ed3ecef750b9feded565c4608d97ca63b6dca
N. Taylor Mullen <nimullen@microsoft.com>
2012-11-26 14:36:19 -0800

    Started working on Auto starting iis for js unit tests

--- tests/Microsoft.AspNet.SignalR.FunctionalTests/Infrastructure/IISExpressTestHost.cs
+++ tests/Microsoft.AspNet.SignalR.FunctionalTests/Infrastructure/IISExpressTestHost.cs
@@ -13,21 +13,45 @@ namespace Microsoft.AspNet.SignalR.FunctionalTests.Infrastructure
         private readonly string _webConfigPath;
         private int _disposed = 0;
 
-        private static readonly Lazy<string> _webConfigTemplate = new Lazy<string>(() => GetConfig());
+        private const string WebConfigTemplate = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<configuration>
+  <appSettings>
+    <add key=""keepAlive"" value=""{0}"" />
+    <add key=""connectionTimeout"" value=""{1}"" />
+    <add key=""heartbeatInterval"" value=""{2}"" />
+    <add key=""enableRejoiningGroups"" value=""{3}"" />
+  </appSettings>
+  <system.web>
+    <compilation debug=""true"" targetFramework=""4.5"" />
+    <httpRuntime targetFramework=""4.5"" />
+  </system.web>
+  <system.webServer>
+    <modules runAllManagedModulesForAllRequests=""true"" />
+  </system.webServer>
+</configuration>";
 
-        public IISExpressTestHost()
+        public IISExpressTestHost() : this(Path.Combine(Directory.GetCurrentDirectory(), ".."))
+        { 
+        }
+
+        public IISExpressTestHost(string path)
         {
             // The path to the site is the test path.
             // We treat the test output path just like a site. This makes it super
             // cheap to create and tear down sites. We don't need to copy any files.
             // The downside is that we can't run tests in parallel anymore.
-            _path = Path.Combine(Directory.GetCurrentDirectory(), "..");
+            _path = path;
 
             // Set the web.config path for this app
             _webConfigPath = Path.Combine(_path, "web.config");
 
             // Create the site manager
             _siteManager = new SiteManager(_path);
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
         }
 
         public string Url { get; private set; }
@@ -44,10 +68,10 @@ namespace Microsoft.AspNet.SignalR.FunctionalTests.Infrastructure
             Url = _siteManager.GetSiteUrl();
 
             // Use a configuration file to specify values
-            string content = String.Format(_webConfigTemplate.Value,
-                                           keepAlive,
-                                           connectonTimeOut,
-                                           hearbeatInterval,
+            string content = String.Format(WebConfigTemplate, 
+                                           keepAlive, 
+                                           connectonTimeOut, 
+                                           hearbeatInterval, 
                                            enableAutoRejoiningGroups);
 
             File.WriteAllText(_webConfigPath, content);
@@ -64,15 +88,6 @@ namespace Microsoft.AspNet.SignalR.FunctionalTests.Infrastructure
         public void Shutdown()
         {
             _siteManager.StopSite();
-        }
-
-        private static string GetConfig()
-        {
-            using (Stream resourceStream = typeof(IISExpressTestHost).Assembly.GetManifestResourceStream("Microsoft.AspNet.SignalR.FunctionalTests.Infrastructure.IIS.site.web.config"))
-            {
-                var reader = new StreamReader(resourceStream);
-                return reader.ReadToEnd();
-            }
         }
     }
 }
